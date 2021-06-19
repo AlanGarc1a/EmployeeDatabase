99249d7ae4b3ea7cd5055dc3a866f0f3ccb6b138
AlanGarc1a <alangarcia969@gmail.com>
2021-06-18 17:14:28 -0700

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+
+const App = () => {
+    return (
+        <Jumbotron fluid>
+            <Container>
+                <h1>Hello World</h1>
+            </Container>
+        </Jumbotron>
+    )
+}
+
+export default App;
