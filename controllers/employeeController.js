364224e8adec287ba85fe39ae480c14194c69270
AlanGarc1a <alangarcia969@gmail.com>
2021-06-23 09:05:50 -0700

    insert/create route successfully inserts employee into database

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -4,14 +4,21 @@
 const connection = require('../config/connection');
 
 /* 
-*   Object controller to export for employee routes
+*   Object controller for employee routes
 *
 *   @createEmployee: adds a new employee to the mysql database
 *
 */
 module.exports = {
     createEmployee: (req, res) => {
-        console.log(req.body);
+        const { firstName, lastName, age, gender, birthday, job } = req.body;
+
+        const INSERT_EMPLOYEE_QUERY = `INSERT INTO employee(first_name, last_name, age, gender, birthday, job) VALUES(?, ?, ?, ?, ?, ?)`;
+
+        connection.query(INSERT_EMPLOYEE_QUERY, [firstName, lastName, age, gender, birthday, job], function(err, results){
+            console.log(results);
+        });
+
         res.status(200).json({msg: "Created employee"});
     }
 
