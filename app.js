db85d7213837ec543e9fd9e3dfc039d008f1944c
AlanGarc1a <alangarcia969@gmail.com>
2021-09-05 12:45:58 -0700

    included the dotenv package

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const cors    = require('cors');
 
@@ -9,7 +10,7 @@ const corsOptions = {
     origin: 'http://localhost:3000',
     optionsSuccessStatus: 200
 };
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const { urlencoded } = require('express');
 
 app.use(cors(corsOptions));
