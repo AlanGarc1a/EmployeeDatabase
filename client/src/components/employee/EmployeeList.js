2d850033baa9ffcbd7ad16f8834aef03f738508c
AlanGarc1a <alangarcia969@gmail.com>
2021-09-26 17:18:24 -0700

    removed Employee component and moved it to its own file

--- client/src/components/employee/EmployeeList.js
+++ client/src/components/employee/EmployeeList.js
@@ -1,68 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import Employee from './Employee';
 import axios from 'axios';
-
-const Employee = ({ id, firstName, lastName, age, gender, birthday, job }) => {
-    return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Body>
-                <Card.Title>{firstName} {lastName}</Card.Title>
-                <Card.Subtitle>Job: {job}</Card.Subtitle>
-                <ListGroup variant="flush">
-                    <ListGroup.Item>Age: {age}</ListGroup.Item>
-                    <ListGroup.Item>Gender: {gender}</ListGroup.Item>
-                    <ListGroup.Item>Birthday: {birthday}</ListGroup.Item>
-                </ListGroup>
-                <Link to={`/edit/${id}`} className="text-white">
-                    <Button variant="warning" className="mr-3">
-                        Edit
-                    </Button>
-                </Link>
-                <Link to={`/employee/${id}`} className="text-white">
-                    <Button className="primary">
-                        View
-                    </Button>
-                </Link>
-            </Card.Body>
-        </Card>
-    )
-}
 
 const EmployeeList = () => {
 
     const [employees, setEmployees] = useState([]);
 
     const fetchEmployees = async () => {
-        const res = await axios.get('http://localhost:5000/api/');
-        setEmployees(res.data);
+        try {
+            const res = await axios.get('/api/employee/');
+            setEmployees(res.data);
+        } catch(error) {
+            console.log(error.response);
+        }
     }
 
     useEffect(() => {
         fetchEmployees();
-    }, []);
+    },[]);
 
     return (
         <Container>
             <Row>
-                {employees.map((employee) => {
+                {employees.map((employee, i) => {
                     return (
-                        <Col className="mt-5" key={employee.id}>
+                        <div className="mt-5 col-xl-4 col-lg-6 col-md-6 col-sm-3 col-xs-12" key={i}>
                             <Employee
                                 id={employee.id}
-                                firstName={employee.first_name}
-                                lastName={employee.last_name}
+                                firstName={employee.firstName}
+                                lastName={employee.lastName}
                                 age={employee.age}
                                 gender={employee.gender}
                                 birthday={employee.birthday}
-                                job={employee.job}
                             />
-                        </Col>
+                        </div>
                     )
                 })}
             </Row>
